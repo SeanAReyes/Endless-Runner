0e0e0d8f2ce420641f97259701e9efda960bae99
SeanAReyes <seanreyes8436@gmail.com>
2026-03-08 00:27:03 -0500

    Added scrolling scene

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private MeshRenderer meshRenderer;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+    private void Update()
+    {
+        float speed = GameManager.Instance.gameSpeed / transform.localScale.x;
+        meshRenderer.material.mainTextureOffset += Vector2.right * speed * Time.deltaTime;
     }
 }
