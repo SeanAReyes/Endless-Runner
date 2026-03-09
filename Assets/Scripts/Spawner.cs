16954692ff48bd4980708fe934860d40e40aab1c
SeanAReyes <seanreyes8436@gmail.com>
2026-03-08 21:10:25 -0400

    Added another enemy and spawner script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public struct SpawnableObject
+    {
+        public GameObject prefab;
+        [Range(0f, 1f)]
+        public float spawnChance;
+    }
+    public SpawnableObject[] objects; 
+}
