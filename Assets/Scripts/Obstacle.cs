e098b07786ccf97c5d95d004ee7bf4c7682de98a
SeanAReyes <seanreyes8436@gmail.com>
2026-03-14 23:13:08 -0400

    Obstacle script completed

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float LeftEdge;
+
+    private void Start()
+    {
+        LeftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 3f;
+    }
+    private void Update()
+    {
+        transform.position += Vector3.left * GameManager.Instance.gameSpeed * Time.deltaTime;
+
+        if (transform.position.x < LeftEdge)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
